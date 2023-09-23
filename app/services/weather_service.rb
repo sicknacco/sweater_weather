490d6244b6bd76f008391cf768b1f8d36cf09d6c
Nick Sacco <nicksacco10@gmail.com>
2023-09-22 20:23:16 -0700

    feat: make connection to weather api

---
+++ app/services/weather_service.rb
@@ -0,0 +1,19 @@
+class WeatherService
+
+  def self.current_weather(lat, lon)
+    get_url("/v1/current.json?q=#{lat},#{lon}")
+  end
+
+  private
+
+  def self.get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.conn 
+    Faraday.new(url: 'http://api.weatherapi.com') do |f|
+      f.params['key'] = Rails.application.credentials.weather[:WEATHER_API_key]
+    end
+  end
+end
