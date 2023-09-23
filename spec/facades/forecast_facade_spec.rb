a285fece7be53720ff1720f1c71de38cb1b1e97e
Nick Sacco <nicksacco10@gmail.com>
2023-09-23 12:42:55 -0700

    test: complete forecast facade happy path tests

--- spec/facades/forecast_facade_spec.rb
+++ spec/facades/forecast_facade_spec.rb
@@ -6,5 +6,11 @@ RSpec.describe 'Forecast facade' do
     weather = ForecastFacade.get_forecast(location)
 
     expect(weather).to be_a(Forecast)
+    expect(weather.id).to eq(nil)
+    expect(weather.type).to eq('forecast')
+    expect(weather.type).to be_a(String)
+    expect(weather.current_weather).to be_a(Hash)
+    expect(weather.daily_weather).to be_a(Array)
+    expect(weather.hourly_weather).to be_a(Array)
   end
 end
