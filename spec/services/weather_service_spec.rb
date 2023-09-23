1fa854358322a0537ad40c783bc128484a292894
Nick Sacco <nicksacco10@gmail.com>
2023-09-22 20:40:06 -0700

    test: complete tests for 5 days forcast

--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe WeatherService do
   describe 'weather_forecast' do
     it 'returns weather data for a given coordinates', :vcr do
       weather = WeatherService.current_weather(39.74001, -104.99202)
-
+      
       expect(weather).to be_a(Hash)
       expect(weather[:location]).to be_a(Hash)
       expect(weather[:location]).to have_key(:name)
@@ -28,6 +28,40 @@ RSpec.describe WeatherService do
       expect(weather[:current][:vis_miles]).to be_a(Float)
       expect(weather[:current]).to have_key(:uv)
       expect(weather[:current][:uv]).to be_a(Float)
+    end
+    
+    it 'returns daily weather data for next 5 days', :vcr do
+      weather = WeatherService.daily_weather(39.74001, -104.99202)
+
+      expect(weather).to be_a(Hash)
+      expect(weather[:forecast]).to be_a(Hash)
+      expect(weather[:forecast]).to have_key(:forecastday)
+      expect(weather[:forecast][:forecastday]).to be_a(Array)
+
+      weather[:forecast][:forecastday].each do |day|
+        expect(day).to be_a(Hash)
+        expect(day).to have_key(:date)
+        expect(day[:date]).to be_a(String)
+        expect(day[:day]).to be_a(Hash)
+        expect(day[:day]).to have_key(:maxtemp_f)
+        expect(day[:day][:maxtemp_f]).to be_a(Float)
+        expect(day[:day]).to have_key(:mintemp_f)
+        expect(day[:day][:mintemp_f]).to be_a(Float)
+
+        expect(day[:day]).to have_key(:condition)
+        expect(day[:day][:condition]).to be_a(Hash)
+        expect(day[:day][:condition]).to have_key(:text)
+        expect(day[:day][:condition][:text]).to be_a(String)
+        expect(day[:day][:condition]).to have_key(:icon)
+        expect(day[:day][:condition][:icon]).to be_a(String)
+
+        expect(day).to have_key(:astro)
+        expect(day[:astro]).to be_a(Hash)
+        expect(day[:astro]).to have_key(:sunrise)
+        expect(day[:astro][:sunrise]).to be_a(String)
+        expect(day[:astro]).to have_key(:sunset)
+        expect(day[:astro][:sunset]).to be_a(String)
+      end
     end
   end
 end
