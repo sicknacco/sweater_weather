b4eb40a614ebb3df33afc4863bf8320e8b9be802
Nick Sacco <nicksacco10@gmail.com>
2023-09-23 14:34:23 -0700

    test: user model validations began

--- app/controllers/api/v0/users_controller.rb
+++ app/controllers/api/v0/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V0::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: UsersSerializer.new(user), status: 201
+      render json: UserSerializer.new(user), status: 201
     else
       render json: { errors: user.errors.full_messages }, status: 400
     end
