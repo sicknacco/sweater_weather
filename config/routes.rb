d37da26fd2f8d045f557634b01d854da4d9287c2
Nick Sacco <nicksacco10@gmail.com>
2023-09-23 17:13:56 -0700

    feat/test: session controller created with full testing with sad path

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v0 do
       resources :forecast, only: [:index]
       resources :users, only: [:create]
+      resources :sessions, only: [:create]
     end
   end
 end
