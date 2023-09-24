5b8e8692681bf2a05c3e051898735068b2f7e454
Nick Sacco <nicksacco10@gmail.com>
2023-09-24 14:35:50 -0700

    test: complete tests for directions service method

--- spec/services/map_service_spec.rb
+++ spec/services/map_service_spec.rb
@@ -14,4 +14,30 @@ RSpec.describe MapService do
       expect(location[:results][0][:locations][0][:latLng][:lng]).to be_a(Float)
     end
   end
+
+  describe 'directions' do
+    it 'returns directions data and weather coordinates for destination', :vcr do
+      directions = MapService.get_directions('cedaredge,co', 'eckert,c0')
+
+      expect(directions).to be_a(Hash)
+      expect(directions[:route]).to be_a(Hash)
+
+      expect(directions[:route]).to have_key(:formattedTime)
+      expect(directions[:route][:formattedTime]).to be_a(String)
+
+      expect(directions[:route]).to have_key(:locations)
+      expect(directions[:route][:locations]).to be_a(Array)
+      expect(directions[:route][:locations][0]).to be_a(Hash)
+      expect(directions[:route][:locations][0]).to have_key(:adminArea5)
+      expect(directions[:route][:locations][0][:adminArea5]).to eq('Cedaredge')
+      expect(directions[:route][:locations][1][:adminArea5]).to eq('Eckert')
+
+      expect(directions[:route][:locations][1]).to have_key(:latLng)
+      expect(directions[:route][:locations][1][:latLng]).to be_a(Hash)
+      expect(directions[:route][:locations][1][:latLng]).to have_key(:lat)
+      expect(directions[:route][:locations][1][:latLng][:lat]).to be_a(Float)
+      expect(directions[:route][:locations][1][:latLng]).to have_key(:lng)
+      expect(directions[:route][:locations][1][:latLng][:lng]).to be_a(Float)
+    end
+  end
 end
