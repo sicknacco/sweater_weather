1791fc001fb339b9de97ec92d710b00de8504a1d
Nick Sacco <nicksacco10@gmail.com>
2023-09-25 09:10:59 -0700

    feat: book facade wip, moving to poro

--- app/facades/books_facade.rb
+++ app/facades/books_facade.rb
@@ -10,8 +10,16 @@ class BooksFacade
       temperature: weather[:current][:temp_f]
     }
 
-    books = BookService.serv_books(location)
-    require 'pry'; binding.pry
-    Books.new(weather, books, location)
+    books = BookService.serv_books(location, quantity)
+    total_books_found = books[:numFound]
+
+    books_data = books[:docs].map do |book|
+      {
+        isbn: book[:isbn],
+        title: book[:title]
+      }
+    end
+    
+    Books.new(forecast, books_data, total_books_found, location)
   end
 end
