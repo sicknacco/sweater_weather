1578a260c2fd005a6e367caa5d0d7afa6a313e4b
Nick Sacco <nicksacco10@gmail.com>
2023-09-25 09:54:37 -0700

    feat/test: complete books poro and beef up facade testing struggled with poro testing

--- spec/facades/books_facade_spec.rb
+++ spec/facades/books_facade_spec.rb
@@ -8,5 +8,17 @@ RSpec.describe BooksFacade do
     book_results = BooksFacade.search_books(location, quantity)
 
     expect(book_results).to be_a(Books)
+    expect(book_results.id).to eq(nil)
+    expect(book_results.type).to eq('books')
+    expect(book_results.destination).to eq(location)
+    expect(book_results.forecast).to be_a(Hash)
+    expect(book_results.forecast[:summary]).to be_a(String)
+    expect(book_results.forecast[:temperature]).to be_a(Float)
+    expect(book_results.total_books_found).to be_a(Integer)
+    expect(book_results.books).to be_a(Array)
+    expect(book_results.books.count).to eq(10)
+    expect(book_results.books.first).to be_a(Hash)
+    expect(book_results.books.first[:isbn]).to be_a(Array)
+    expect(book_results.books.first[:title]).to be_a(String)
   end
 end
--- spec/poros/books_spec.rb
+++ spec/poros/books_spec.rb
@@ -2,9 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Books do
   describe 'initialize' do
-    it 'exists with attributes' do
-      books_data = Books.new("denver,co", {:summary=>"Sunny", :temperature=>80}, 80, [{:isbn=>["0762507845", "9780762507849"], :title=>"Denver, Co"}])
-      require 'pry'; binding.pry
+    before(:each) do
+      @forecast = { summary: 'Sunny', temperature: 75.0 }
+      @books_data = [
+        { isbn: '123456', title: 'Book 1' },
+        { isbn: '789012', title: 'Book 2' },
+      ]
+      @total_books_found = 10
+      @location = 'denver,co'
+    end
+
+    xit 'exists with attributes' do
+      books = Books.new(@forecast, @books_data, @total_books_found, @location)
+      
     end
   end
 end
